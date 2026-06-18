64c4e04a9b2cad20382b41c9ecd8cf509bbb1048
anyable <stupiranna@mail.ru>
2026-06-18 14:08:53 +0300

    add credentials

---
+++ stupir.anna/S0/credentials.cpp
@@ -0,0 +1,7 @@
+#include "credentials.hpp"
+#include <iostream>
+
+std::ostream & stupir::out_credentials(std::ostream & out)
+{
+  return out << "stupir.anna";
+}
--- stupir.anna/S0/main.cpp
+++ stupir.anna/S0/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "credentials.hp
+
 int main()
 {
-  std::cout << "stupir.anna\n";
+  stupir::out_credentials(std::cout) << "\n";
 }
